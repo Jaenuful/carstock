080afb8b96b54678fcf63fba73db4df2f22e791b
Willimann <jwillimann@novabio.com>
2020-09-05 16:53:19 +0200

    new datat

--- settings.py
+++
@@ -1,6 +0,0 @@
-from os import environ 
-
-SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
-SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
-SECRET_KEY = environ.get('SECRET_KEY')
-#SECRET_KEY = environ.get('SECRET_KEY')
