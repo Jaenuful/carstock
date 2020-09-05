b984fe3436ad521d8d758dacf0a599e6fa935c7d
Willimann <jwillimann@novabio.com>
2020-09-05 16:56:17 +0200

    .

---
+++ README.md
@@ -0,0 +1,13 @@
+# Carstock
+
+## Usage
+
+Activate virtual env.
+
+Create `.env` file with `DATABASE_URL`.
+
+Install dependencies.  
+`pip install -r requirements.txt`
+
+Start the app.  
+`flask run`
---
+++ settings.py
@@ -0,0 +1,6 @@
+from os import environ 
+
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+SECRET_KEY = environ.get('SECRET_KEY')
+#SECRET_KEY = environ.get('SECRET_KEY')
